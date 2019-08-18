eaff381847eaabe83d63c778449e699b4eba903e
Devin Jordao <devinjordao@gmail.comgit config --global user.name Devin>
2019-08-18 05:51:56 +0000

    Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :enrolled_in_current_course, only: :show
 
   def show
   end
 
   private
+
+  def enrolled_in_current_course
+    if current_user != current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path, alert: 'Not Authorized'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
